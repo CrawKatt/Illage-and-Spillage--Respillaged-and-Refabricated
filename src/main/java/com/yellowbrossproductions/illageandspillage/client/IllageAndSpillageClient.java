deef643aef7ecff046a318056ae2cbe434c3c57f
crawkatt <man849916@gmail.com>
2026-08-19 16:05:23 -0400

    Add ClientModIinitializer class

---
+++ src/main/java/com/yellowbrossproductions/illageandspillage/client/IllageAndSpillageClient.java
@@ -0,0 +1,10 @@
+package com.yellowbrossproductions.illageandspillage.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class IllageAndSpillageClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
